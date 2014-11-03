ac93989aff7dfbfa1f9435e69edd29f4f95ecb60
AlbertoCejas <Alberto.Cejas.Sanchez@gmail.com>
2014-11-03 20:49:07 +0100

    Adds empty projects

---
+++ theplane-basic/desktop/src/uca/workshop/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package uca.workshop.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import uca.workshop.game.PlaneGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PlaneGame(), config);
+	}
+}
